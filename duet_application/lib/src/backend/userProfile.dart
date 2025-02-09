b273e3df9c3ef1aa22fca9b3a90be91420b60af2
Selim <sesaridede3@gmail.com>
2025-02-08 17:12:52 -0800

    Added uuid to pubspec.yaml

--- duet_application/lib/src/backend/userProfile.dart
+++ duet_application/lib/src/backend/userProfile.dart
@@ -2,10 +2,24 @@
   *
   *
   */
-class UserProfileData {
-  //String uuid;
-  //String name, location;
 
-  UserProfileData({ dynamic uuid = ""});
+import 'package:uuid/uuid.dart';
+
+class UserProfileData {
+  final String _uuid;  // Private unique user ID
+  final String name, dob, location; // Public fields: name, date of birth, location
+  
+  // Create an instance of the Uuid class
+  static final _uuidGen = Uuid();
+
+  // Constructor Method
+  UserProfileData({
+    required this.name,
+    required this.dob,
+    required this.location
+  }) :   this._uuid = _uuidGen.v4(); // Generate and assign a UUID
+
+  // Getter for _uuid (to allow read access)
+  String get uuid => _uuid;
 
 }
